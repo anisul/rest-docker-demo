aca4f54319654e06ec93f880ee59beeadff13256
anisul <anis.2803@gmail.com>
2018-12-05 17:29:50 +0100

    Initial commit

---
+++ src/main/java/com/perccom/restdockerdemo/RestDockerDemoApplication.java
@@ -0,0 +1,14 @@
+package com.perccom.restdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class RestDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDockerDemoApplication.class, args);
+	}
+}
